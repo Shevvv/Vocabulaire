2ec951d82dbe560113af836b3e575ae4205ba1bc
Shevvv <34458530+Shevvv@users.noreply.github.com>
2023-12-31 00:16:44 +0100

    v0.7.1 allowed hosts

--- Vocabulaire/settings.py
+++ Vocabulaire/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%l-_o*%#q0x0xmi9b(v&f%9x%yuhm2&18qf7nez06@iv#xh8wn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['learnrussian.pythonanywhere.com']
 
 
 # Application definition
